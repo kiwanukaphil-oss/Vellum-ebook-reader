58594a64f400a3ab3a881f7dfe4319bc2f185706
Pkiwan <kiwanukaphil@gmail.com>
2026-08-24 22:50:19 +0300

    Prefer IPv6 for Railway REST listener

--- .railway/railway.ts
+++ .railway/railway.ts
@@ -102,7 +102,7 @@ export default defineRailway((railwayContext) => {
     healthcheckTimeout: 120,
     env: {
       PORT: "3000",
-      PGRST_SERVER_HOST: "*",
+      PGRST_SERVER_HOST: "*6",
       PGRST_DB_URI:
         "postgres://authenticator:${{shared.POSTGRES_PASSWORD}}@${{db.RAILWAY_PRIVATE_DOMAIN}}:5432/postgres",
       PGRST_DB_SCHEMAS: "public",
